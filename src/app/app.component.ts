1e4b27e6c04c918a4150bddbadbd0d67430aad54
Francisco Eduardo <franciscoeduardo88@gmail.com>
2022-05-03 10:16:45 -0300

    Add backup and restore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { StorageService } from './../services/storage.service';
+import { Component, EventEmitter } from '@angular/core';
 import { Icons } from './global-components/Icons.enum';
 
 @Component({
@@ -14,7 +15,33 @@ export class AppComponent  {
 
   icons: typeof Icons = Icons;
 
+  constructor(private storageService: StorageService) { }
+
   onSubmit() {
     console.log("Evento chamado pelo pai");
+  }
+
+  backup() {
+    this.storageService.backup();
+  }
+
+  restore(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length > 0) {
+      var reader = new FileReader();
+      var nameFile = target.files[0].name;
+      reader.readAsText(target.files[0], "UTF-8");
+      reader.onload = (evt : any ) => {
+        if (confirm("Deseja importar o arquivo " + nameFile + "?")) {
+          this.storageService.restore(evt.target.result);
+        }
+      }
+      reader.onerror = (evt) => {
+        alert("Erro ao importar arquivo");
+      }
+
+
+    }
+
   }
 }
--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -18,4 +18,29 @@ export class StorageService {
     window.localStorage.setItem(base, strValue);
   }
 
+  public backup() {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(window.localStorage));
+    var date = new Date();
+    var dateStr = date.toLocaleDateString("pt-br").split("/").reverse().join("-")+"_"+ date.toLocaleTimeString("pt-BR")
+    var virtualElement = document.createElement('a' );
+    virtualElement.setAttribute("href",     dataStr     );
+    virtualElement.setAttribute("download", "financeiro-"+dateStr+".json");
+    document.body.appendChild(virtualElement);
+    virtualElement.click();
+    virtualElement.remove();
+  }
+
+  public restore(baseStr: string) {
+    var base = JSON.parse(baseStr);
+    window.localStorage.clear();
+    for (const key in base) {
+      if (Object.prototype.hasOwnProperty.call(base, key)) {
+        const element = base[key];
+        window.localStorage.setItem(key, element);
+      }
+    }
+    window.location.reload();
+
+  }
+
 }
